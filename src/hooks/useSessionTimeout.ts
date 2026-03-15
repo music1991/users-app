2dcf96149fc7a13096ea70c43591c34df1101383
music1991 <sebastians201491@gmail.com>
2026-03-15 14:27:20 -0300

    chore: remove innecesary dependecies

--- src/hooks/useSessionTimeout.ts
+++ src/hooks/useSessionTimeout.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { jwtDecode, JwtPayload } from "jwt-decode";
 import { toast } from 'react-toastify';
 import { useAuth } from "../context/AuthContext";
